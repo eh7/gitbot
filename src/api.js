2aba63a8c0005371c5173143a4fd541aa6b4e1ca
Gavin Francis <gav@eh7.co.uk>
2021-05-06 16:55:09 +0000

    adding TLS listen for api.cgi

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 
+var fs = require('fs');
+var https = require("https"); 
 var express = require("express"); 
 var bodyParser = require('body-parser');
 var app = express(); 
-var fs = require('fs');
-
 
 //console.log(Date());
 //process.exit();
@@ -19,12 +19,8 @@ const CHART_ID = process.env.TELEGRAM_CHAT_ID;
 const gitHubHook = new GitHubHook(TOKEN, CHART_ID);
 
 app.get('/', function(req,res){ 
-  res.send("<h2>Hi there, thanks for checking out my work. The server that is responding to you is developed using 'express.js' module in node.js. Please type 'localhost:3000/one' in your URL search bar to view the next page.</h2>");
+  res.send("<h2>Your names not down, your not getting in! Peace. :) </h2>");
 });  
-
-app.get('/one', function(req,res){ 
-  res.send("<h2>Have a nice day</h2>");
-});
 
 app.post('/hook', async function(req,res){ 
 
@@ -35,9 +31,6 @@ app.post('/hook', async function(req,res){
   await gitHubHook.reqBody(req.body);
   // await gitHubHook.evresp('req.body');
 
-  // gitHubHook.evresp('issues');
-
-//  const logData = new Date().getTime() +
   const logData = new Date().toISOString() +
     ' (' + ip + '): ' +
     JSON.stringify(req.body, null, 0) + '\n';
@@ -48,6 +41,24 @@ app.post('/hook', async function(req,res){
   });
 });
 
-app.listen(3000,function(){ 
-  console.log("Server listening on port: 3000");
-});
+if (process.env.HTTPS) {
+  const privateKey  = fs.readFileSync(
+    '/home/gavin/certbot/config/live/vps00.eh7.co.uk/privkey.pem',
+    'utf8'
+  );
+  const certificate = fs.readFileSync(
+    '/home/gavin/certbot/config/live/vps00.eh7.co.uk/cert.pem',
+    'utf8'
+  );
+  https.createServer({
+    key: privateKey,
+    cert: certificate
+  }, app).listen(3000, "0.0.0.0", function() { 
+    console.log("TLS Server listening on port: 3000");
+  });
+} else {
+  app.listen(3000,function(){ 
+    console.log("Server listening on port: 3000");
+  });
+}
+
